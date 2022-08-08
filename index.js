68f1d6d3bc357372ee1542007c97df55a8ae5b6d
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-08-08 17:46:05 -0500

    req.ip

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+  console.log(req.ip)
 });
+
+app.use('/api/whoami', function (req, res, next) {
+  res.json({ ipaddress: req.ip })
+  next();
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
